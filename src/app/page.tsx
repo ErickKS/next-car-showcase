96cda263f4d00b5f71184f1cc9df98a510bf4840
Erick <erick.kuwahara79@gmail.com>
2023-06-11 16:03:49 -0300

    feat: search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Banner from "@/components/Banner";
+import CustomFilter from "@/components/CustomFilter";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Banner />
+
+      <div className="mx-auto mt-12 max-w-[1440px] px-6 py-4 sm:px-16" id="discover">
+        <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="flex-between mt-12 w-full flex-wrap items-center gap-5">
+          <SearchBar />
+
+          <div className="flex flex-wrap items-center justify-start gap-2">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/SearchManufacturer.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image from "next/image";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+
+import { manufacturers } from "@/constants";
+
+interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export default function SearchManufacturer({ manufacturer, setManufacturer }: SearchManufacturerProps) {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item.toLocaleLowerCase().replace(/\s+/g, "").includes(query.toLocaleLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="flex flex-1 items-center justify-start max-sm:w-full">
+      <Combobox value={manufacturer} onChange={setManufacturer}>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image src="/car-logo.svg" alt="Car logo" width={20} height={20} className="ml-4" />
+          </Combobox.Button>
+
+          <Combobox.Input
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+            className="h-[48px] w-full cursor-pointer rounded-l-full bg-light-white p-4 pl-12 text-sm outline-none max-sm:rounded-full"
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  value={item}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4
+                    ${active ? "bg-primary-blue text-white" : "text-gray-900"}`
+                  }
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{item}</span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        />
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
