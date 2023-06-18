310c1f17f53acbe511f3269d059adb600f85794b
Erick <erick.kuwahara79@gmail.com>
2023-06-18 13:32:06 -0300

    style: search bar

--- src/components/SearchManufacturer.tsx
+++ src/components/SearchManufacturer.tsx
@@ -43,7 +43,10 @@ export default function SearchManufacturer({ manufacturer, setManufacturer }: Se
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options>
+            <Combobox.Options
+              className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              static
+            >
               {filteredManufacturers.map((item) => (
                 <Combobox.Option
                   key={item}
